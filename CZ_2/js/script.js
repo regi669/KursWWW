ab1077642495e46ebe04307efe9f694b0249f050
Szymon <regicsmaster@gmail.com>
2022-08-13 17:50:22 +0200

    JS classList

--- CZ_2/js/script.js
+++ CZ_2/js/script.js
@@ -1,16 +1,24 @@
 
 
-const btn1 = document.querySelector('.btn1')
+const red = document.querySelector('#btn1');
+const blue = document.querySelector('.btn2');
+const revertColor = document.querySelector('.remove-color');
+const colorDiv = document.querySelector('.color');
 
-const btn2 = document.querySelector('.btn2')
-
-function HandleClick () {
-    console.log("Btn1 Was Clicked")
+function colorRed () {
+    colorDiv.classList.toggle('red')
 }
 
-function HandleClick2 () {
-    console.log("Btn2 Was Clicked")
+function colorBlue () {
+    colorDiv.classList.add('blue');
+    colorDiv.classList.remove('red');
 }
 
-btn1.addEventListener('click', HandleClick)
-btn2.addEventListener('click', HandleClick2)
+function revertColorF () {
+    colorDiv.classList.remove('red');
+    colorDiv.classList.remove('blue');
+}
+
+red.addEventListener('click', colorRed)
+blue.addEventListener('click', colorBlue)
+revertColor.addEventListener('click', revertColorF)

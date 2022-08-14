ff1433f31b8332eed40a5f57ff9980f0c0830eac
Szymon <regicsmaster@gmail.com>
2022-08-14 09:39:14 +0200

    JS Reveal Img

--- CZ_2/js/script.js
+++ CZ_2/js/script.js
@@ -1,24 +1,14 @@
+const imgButton = document.querySelector('.arrow')
+const imgToChangeSize = document.querySelector('.item1')
 
-
-const red = document.querySelector('#btn1');
-const blue = document.querySelector('.btn2');
-const revertColor = document.querySelector('.remove-color');
-const colorDiv = document.querySelector('.color');
-
-function colorRed () {
-    colorDiv.classList.toggle('red')
+function onClick () {
+    if (imgToChangeSize.classList.contains('show')) {
+        imgToChangeSize.classList.remove('show');
+        imgButton.classList.remove('animate');
+    } else {
+        imgToChangeSize.classList.add('show');
+        imgButton.classList.add('animate');
+    }
 }
 
-function colorBlue () {
-    colorDiv.classList.add('blue');
-    colorDiv.classList.remove('red');
-}
-
-function revertColorF () {
-    colorDiv.classList.remove('red');
-    colorDiv.classList.remove('blue');
-}
-
-red.addEventListener('click', colorRed)
-blue.addEventListener('click', colorBlue)
-revertColor.addEventListener('click', revertColorF)
+imgButton.addEventListener('click', onClick)

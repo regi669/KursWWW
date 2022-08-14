c6eaa75472632e1679af50f6db72e2cd31e28074
Szymon <regicsmaster@gmail.com>
2022-08-14 22:02:37 +0200

    About us finished

--- CZ_2/js/script.js
+++ CZ_2/js/script.js
@@ -3,11 +3,39 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log(nav)
 
     function addShadow() {
-        if (window.scrollY >= 100) {
+        if (window.scrollY >= 100 || window.innerWidth <= 400) {
             nav.classList.add('shadow-bg');
         } else {
             nav.classList.remove('shadow-bg')
         }
     }
+    addShadow()
     window.addEventListener('scroll', addShadow)
+
+    const navbarMobile = document.querySelector('#navbarNavAltMarkup')
+    const navLinks = document.querySelectorAll('.nav-link')
+    const icon = document.querySelector('.fa-bars')
+    const toggler = document.querySelector('.navbar-toggler')
+    console.log(icon)
+
+    navLinks.forEach(navLink => navLink.addEventListener('click', () => {
+            if (navbarMobile.classList.contains('show')) {
+                navbarMobile.classList.remove('show');
+                icon.classList.add('fa-bars')
+                icon.classList.remove('fa-caret-down')
+            } else {
+
+            }
+        })
+    )
+
+    toggler.addEventListener('click', function () {
+        if (icon.classList.contains('fa-bars')) {
+            icon.classList.remove('fa-bars')
+            icon.classList.add('fa-caret-down')
+        } else {
+            icon.classList.add('fa-bars')
+            icon.classList.remove('fa-caret-down')
+        }
+    })
 });

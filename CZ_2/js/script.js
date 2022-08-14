903d603c3a7c6562fcfcfeb71417d1219a0d0ccc
Szymon <regicsmaster@gmail.com>
2022-08-14 09:54:25 +0200

    JS + - color

--- CZ_2/js/script.js
+++ CZ_2/js/script.js
@@ -1,14 +1,24 @@
-const imgButton = document.querySelector('.arrow')
-const imgToChangeSize = document.querySelector('.item1')
+const plusButton = document.querySelector('.size-up');
+const minusButton = document.querySelector('.size-down');
+const color = document.querySelector('.color');
+const p = document.querySelector('p');
 
-function onClick () {
-    if (imgToChangeSize.classList.contains('show')) {
-        imgToChangeSize.classList.remove('show');
-        imgButton.classList.remove('animate');
-    } else {
-        imgToChangeSize.classList.add('show');
-        imgButton.classList.add('animate');
-    }
+let fontsize = 20;
+
+function bigText () {
+    fontsize++;
+    p.style.fontSize = fontsize +  'px'
 }
 
-imgButton.addEventListener('click', onClick)
+function smallText () {
+    fontsize--;
+    p.style.fontSize = fontsize +  'px'
+}
+
+function changeColor () {
+    p.style.color = p.style.color === 'gold' ? 'white' : 'gold';
+}
+
+plusButton.addEventListener('click', bigText)
+minusButton.addEventListener('click', smallText)
+color.addEventListener('click', changeColor)

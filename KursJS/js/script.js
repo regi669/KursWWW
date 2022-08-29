baddc39ff5ed6b1800635cb4a72d6cf34adae59b
Szymon <regicsmaster@gmail.com>
2022-08-29 14:44:22 +0200

    js start

---
+++ KursJS/js/script.js
@@ -0,0 +1,9 @@
+const test = document.getElementById('test')
+console.log(test)
+const test2 = document.getElementsByTagName('ul')
+console.log(test2)
+const test3 = document.getElementsByClassName('test3')
+console.log(test3)
+
+const newTest = document.querySelector('#test') //nazwa klasy w css
+console.log(newTest)
